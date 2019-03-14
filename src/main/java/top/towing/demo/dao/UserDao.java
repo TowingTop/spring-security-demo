bcf77edf842e82bfc5312baa77aebe618ad80bd4
Tong <ytong@ebay.com>
2019-03-15 00:24:05 +0800

    Refactor Code, 1st Workable Version

--- src/main/java/top/towing/demo/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package top.towing.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import top.towing.demo.model.User;
-
-public interface UserDao extends JpaRepository<User, String> {
-	User findByUsername(String userId);
-}
